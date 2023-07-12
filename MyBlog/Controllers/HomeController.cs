e2b165d71bfc48518acdd603f193d8d85f03e4ae
JoonGit <ljj9730@gmail.com>
2023-07-13 08:49:20 +0900

    first commit

---
+++ MyBlog/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using MyBlog.Models;
+using System.Diagnostics;
+using System.IO;
+
+namespace MyBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //private readonly IHostingEnvironment environment;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }       
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task Upload(IFormFileCollection files)
+        {
+            string uploadUrl = "http://13.124.189.61/img";
+            using (var httpClient = new HttpClient())
+            {
+                using (var form = new MultipartFormDataContent())
+                {
+                    foreach(var file in files)
+                    {
+                        using (var fileStream = file.OpenReadStream())
+                        {
+                            var fileName = Path.GetFileName(file.FileName);
+                            var fileContent = new StreamContent(fileStream);
+
+                            // 파일 컨텐츠를 MultipartFormDataContent에 추가
+                            form.Add(fileContent, "file", fileName);
+
+                            // 서버에 요청을 보내고 응답을 받음
+                            var response = await httpClient.PostAsync(uploadUrl, form);
+
+                            // 응답 처리
+                            if (response.IsSuccessStatusCode)
+                            {
+                                Console.WriteLine("파일 업로드 성공");
+                            }
+                            else
+                            {
+                                Console.WriteLine("파일 업로드 실패");
+                            }
+
+                            //using (Stream fileStream = fileInfo.OpenRead())
+                            //{
+                            //    // 스트림 이동
+                            //    fileStream.CopyTo(stream);
+                            //}
+                            // stream을 byte로 변환할 버퍼 생성
+                            //var ret = new byte[stream.Length];
+                            //// 스트림 seek 이동
+                            //stream.Seek(0, SeekOrigin.Begin);
+                            //// stream을 byte로 이동
+                            //stream.Read(ret, 0, ret.Length);
+
+
+
+
+                            HttpClient client = new HttpClient();
+                            HttpResponseMessage response2 = await client.GetAsync(uploadUrl);
+                            byte[] responseContent = await response2.Content.ReadAsByteArrayAsync();
+                            //await System.IO.File.Create(responseContent, "C:\img\mask.png");
+                            //await System.IO.File.WriteAllBytesAsync(@"C:\img\mask.png", responseContent);
+                        }
+
+                    }
+                    
+                }
+            }
+        }
+
+
+
+
+
+        //public IActionResult Upload(ProductModel model, IFormFileCollection files)
+        //{
+        //    try
+        //    {
+
+        //        foreach (var file in files)
+        //        {
+        //            if (file.Length > 0)
+        //            {
+
+        //                string fileName = Path.GetFileName(Convert.ToString(file.FileName));
+        //                // 경로에 파일이 없으면 생성
+        //                //string path = "./test";
+        //                string path = "./" + model.RegisterUserId;
+        //                if (!Directory.Exists(path))
+        //                {
+        //                    Directory.CreateDirectory(path);
+        //                }
+        //                string filePath = Path.Combine(path, fileName);
+        //                using (var fileStream = new FileStream(filePath, FileMode.Create))
+        //                {
+        //                    file.CopyTo(fileStream);
+        //                }
+        //            }
+        //        }
+        //        return Redirect("/home");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // 파일 업로드 실패 처리
+        //        Console.WriteLine(ex.Message);
+        //        return Redirect("/home");
+        //    }
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MyBlog/Controllers/ProductController.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBlog.Data;
+using MyBlog.Data.Service;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    [Route("Product")]
+    public class ProductController : Controller
+    {
+
+        private readonly IProudctesService _service;
+        private readonly UserManager<NewIdentityUser> _userManager;
+        private readonly BlogDbContext _dbContext;
+
+
+        public ProductController(
+            IProudctesService service
+            , UserManager<NewIdentityUser> userManager
+            , BlogDbContext dbContext
+
+            )
+        {
+            _service = service;
+            _userManager = userManager;
+            _dbContext = dbContext;
+        }
+
+        #region 상품등록
+        [HttpGet("register")]
+        public async Task<IActionResult> Register()
+        {
+            // 사용자 정보는 나중에 캐시나 세션으로 값 받아오는 걸로 해결
+            ViewBag.userId =_userManager.GetUserId(HttpContext.User);
+            return View();
+        }
+
+        // 권한을 소비자 등록자 로 나워 등록자만 접근 가능하도록 변경
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(ProductModel model, IFormFileCollection files)
+        {
+            // 값 hidden
+            //if (!ModelState.IsValid) return View(model);
+            //model.RegisterUserId = _userManager.GetUserId(HttpContext.User);
+            // hidden으로 받은 값으로 경로 설정
+
+            if(await _service.Upload(model, files) > 0)
+            {
+                return View(model);
+            }
+            // 파일 업로드후 모델 저장
+            await _service.AddAsync(model);
+            return Redirect("/product/list");  
+        }
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+        #region 상품목록조회
+        [HttpGet("list")]
+        public async Task<IActionResult> List()
+        {
+            // 전체 상품 목록 조회
+            var result = _dbContext.Products.ToList();
+            return View(result);
+        }
+        #endregion
+
+        #region 상품상세조회
+
+        [HttpGet("read/{id:int}")]
+        public async Task<IActionResult> Read(int id)
+        {
+            // 상품의 Id로 해당 물건 정보 가져오기
+            var result = _service.GetByIdAsync(id);
+            return View(result);
+        }
+        #endregion
+
+        #region 상품수정
+        [HttpGet("Edit/{id:int}")]
+        public IActionResult Edit(int id)
+        {
+            // 수정할 상품 정보 불러오기
+            var result = _service.GetByIdAsync(id);
+            return View(result.Result);
+        }
+        [HttpPost("Edit")]
+        public IActionResult Edit(ProductModel model)
+        {
+            // 변경사항 없으면 변경 X
+            var UpdateModel = _dbContext
+                            .Products
+                            .Where(product => product.Id == model.Id)
+                            .FirstOrDefault();
+
+            UpdateModel.Name = model.Name;
+            UpdateModel.Price = model.Price;
+
+            if(UpdateModel.URI != model.URI)
+            {
+                Delete(model.Id);
+                //RegisterAsync()
+            }
+
+            _dbContext.SaveChanges();
+            // 필요한 부분만 변경을 원하면
+            //_service.UpdateAsync(Id, model);
+            return Redirect("/product/list");
+        }
+        #endregion
+
+        #region 상품삭제
+        // 상품 삭제
+        [HttpGet("delete/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            // 상품 삭제시 파일도 삭제
+            var result = _service.GetByIdAsync(id);
+            // 파일 삭제
+            string path = @"wwwroot/" + result.Result.URI;
+            if (System.IO.File.Exists(path))
+            {
+                try
+                {
+                    System.IO.File.Delete(path);
+                }
+                catch (System.IO.IOException e)
+                {
+                    await Console.Out.WriteLineAsync(e.Message);
+                    // handle exception
+                }
+            }
+
+            // 상품 삭제
+            await _service.DeleteAsync(id);
+            
+            return Redirect("/product/list");
+        }
+        #endregion
+
+        #region 장바구니
+        [HttpGet("buy/{id:int}")]
+        public async Task<IActionResult> buy(int id)
+        {
+            var user = HttpContext.User;
+            // 현재 유저의 구매 이력 만들기 
+            var curUser = await _userManager.GetUserAsync(user);
+            BuyListModel buyList = new BuyListModel()
+            {
+                // 현재 구매하는 상품의 id
+                ProductModelId = id,
+                // 구매하는 유저의 id
+                NewIdentityUserId = curUser.Id,
+            };
+            _dbContext.Add(buyList);
+            _dbContext.SaveChanges();
+            return Redirect("/product/list");
+        }
+        #endregion
+
+        #region 상품 찜 하기
+        // 상품 찜 하기
+        [HttpGet("Wish/{id:int}")]
+        public async Task<string> Wish(int id)
+        {
+            var user = HttpContext.User;
+            // 상품의 id와 유저의 정보를 확인해 찜하기, 정보가 있으면 취소
+
+            var curUser = await _userManager.GetUserAsync(user);
+
+            // 찜을 누른 상품과 좋아요를 누른 사람의 목록 가져오기
+            var result = _dbContext.Products.Include(p => p.WishUsers).Where(p => p.Id == id).FirstOrDefault();
+
+            if (result.WishUsers.Count == 0)
+            {
+                result.WishUsers.Add(curUser);
+                _dbContext.SaveChanges();
+                return "ok";
+            }
+            else
+            {
+                result.WishUsers.Remove(curUser);
+                _dbContext.SaveChanges();
+                return "remove";
+            }
+        }
+        #endregion
+    }
+}
